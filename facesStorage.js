2348ff29084a937d155b78572af97b3ad9fccc40
d4rk3c <39152557+d4rk3c@users.noreply.github.com>
2018-08-21 19:27:03 +0200

    version 1.0

---
+++ facesStorage.js
@@ -0,0 +1,71 @@
+var fsstart = {
+    loc: "unknown",
+    answer: "unknown"
+}
+var fs0 = {
+    loc: "./facesImages/milosTeodosic.jpg",
+    answer: "milos teodosic"
+
+}
+var fs1 = {
+    loc: "./facesImages/nenadJezdic.jpg",
+    answer: "nenad jezdic"
+};
+var fs2 = {
+    loc: "./facesImages/djordjeBalasevic.jpg",
+    answer: "djordje balasevic"
+};
+var fs3 = {
+    loc: "./facesImages/dzoniStulic.jpg",
+    answer: "dzoni stulic"
+};
+var fs4 = {
+    loc: "./facesImages/vladislavPetkovicDis.jpg",
+    answer: "vladislav petkovic dis"
+};
+var fs5 = {
+    loc: "./facesImages/anicaDobra.jpg",
+    answer: "anica dobra",
+};
+var fs6 = {
+    loc: "./facesImages/branislavLecic.jpg",
+    answer: "branislav lecic",
+};
+var fs7 = {
+    loc: "./facesImages/nikolaKojo.jpg",
+    answer: "nikola kojo",
+};
+var fs8 = {
+    loc: "./facesImages/zdravkoColic.jpg",
+    answer: "zdravko colic",
+};
+var fs9 = {
+    loc: "./facesImages/vlatkoStefanovski.jpg",
+    answer: "vlatko stefanovski",
+};
+var fs10 = {
+    loc: "./facesImages/ivoAndric.jpg",
+    answer: "ivo andric",
+};
+var fs11 = {
+    loc: "./facesImages/mesaSelimovic.jpg",
+    answer: "mesa selimovic",
+};
+var fs12 = {
+    loc: "./facesImages/patrijarhPavle.jpg",
+    answer: "patrijarh pavle",
+};
+var fs13 = {
+    loc: "./facesImages/miroslavRaduljica.jpg",
+    answer: "miroslav raduljica",
+};
+var fs14 = {
+    loc: "./facesImages/ivanaMaksimovic.jpg",
+    answer: "ivana maksimovic",
+};
+var fs15 = {
+    loc: "./facesImages/leaDobricic.jpg",
+    answer: "lea dobricic",
+};
+
+var finalPhotos = [fsstart,fs0,fs1,fs2,fs3,fs4,fs5,fs6,fs7,fs8,fs9,fs10,fs11,fs12,fs13,fs14,fs15];
---
+++ questionStorage.js
@@ -0,0 +1,206 @@
+
+
+var q1 = {
+    question: "Koju zemlju popularno nazivaju 'zemlja gejzira'?",
+    answer: "island",
+    points: 10
+};
+var q2 = {
+    question: "Koja ja drzava u Evropi najveci prozvodjac banana?",
+    answer: "spanija"
+}
+var q3 = {
+    question: "Mestici nastaju iz brakova belaca i?",
+    answer: "indijanaca"
+}
+var q4 = {
+    question: "Koliko reka protice kroz Kraljevo(recima napisati)",
+    answer: "tri"
+};
+var q5 = {
+    question: "U okolini Šapca postoji selo koje ima isto ime kao jedan veći grad u Bugarskoj. Kako se zove taj grad?",
+    answer: "varna"
+};
+var q6 = {
+    question: "Odakle vuku poreklo 'mnogoljudni' Kinezi ? ",
+    answer: "iz afrike"
+};
+var q7 = {
+    question: "Koja zemlja ima najmanje stanovnika na svetu? ",
+    answer: "vatikan"
+};
+var q8 = {
+    question: "Kako se zove jedino more na svetu koje nema obale ? ",
+    answer: "sargasko more"
+};
+var q9 = {
+    question: " Koja država ima najdužu obalu na svetu? ? ",
+    answer: "kanada"
+};
+var q10 = {
+    question: "Koja zemljina polulopta je toplija? ",
+    answer: "severna"
+};
+var q11 = {
+    question: "Koji okean može da se prepešači? ",
+    answer: "severni ledeni okean"
+};
+var q12 = {
+    question: "Kroz koliko časovnih zona se proteže Rusija?(napisati broj slovima)",
+    answer: "jedanaest"
+};
+var q13 = {
+    question: "Koja planeta ima najveci sjaj?",
+    answer: "venera"
+};
+var q14 = {
+    question: "Koja se zove najukrašenija (od strane ljudi) pećina na svetu?",
+    answer: "lesko"
+};
+var q15 = {
+    question: "Koja je zemlja prva u svetu po rezervama pitke vode?",
+    answer: "island"
+};
+var q16 = {
+    question: "Koja oblast se naziva 'pluća Zemlje'?(dve reci, mnozina)",
+    answer: "amazonske sume"
+};
+var q17 = {
+    question: "Koja država ima najveću količinu zemlje crnice na svetu? ? ",
+    answer: "ukraina"
+};
+var q18 = {
+    question: "Koji je najveci divlji biljojed u Evropi? ",
+    answer: "bizon"
+};
+var q19 = {
+    question: "Koji je najveci divlji mesojed u Evropi?? ",
+    answer: "medved"
+};
+var q20 = {
+    question: "Koja zemlja je najveci proizvodjac secerne trske?? ",
+    answer: "kuba"
+};
+var q21 = {
+    question: "U koliko država se vozi levom stranom kolovoza?(napisati broj)",
+    answer: 54
+};
+var q22 = {
+    question: "koji jezik je najzastupljeniji na svetu, po tome koliko ljudi ga govori i kako se tacno zove?  ",
+    answer: "mandarinski kineski"
+};
+var q23 = {
+    question: "Glavni grad Izraela je?",
+    answer: "jerusalim"
+};
+var q24 = {
+    question: "Koja zemlja ima najvecu gustinu naseljenosti? ",
+    answer: "monako"
+};
+var q25 = {
+    question: "Kako se zove sport gde ekipe broje 4 - 6 članova i takmiče se u disciplinama slalom, sprint ili spust",
+    answer: "rafting"
+};
+var q26 = {
+    question: "Kako se zove grad u kom su odrzane prve moderne Olimpijske Igre?",
+    answer: "atina"
+};
+var q27 = {
+    question: "Najmnogoljudnija afrička država je...?",
+    answer: "nigerija"
+};
+var q28 = {
+    question: "Godine 1870. su Beograd i Zagreb imali tek nekoliko desetina hiljada duša. Koji je grad bio najveći u to vreme?",
+    answer: "subotica"
+};
+var q29 = {
+    question: "Koja drzava u Evropi se nalazi ispod nivoa mora?",
+    answer: "holandija"
+};
+var q30 = {
+    question: "Koja je drzava bila prva ateisticka drzava u svetu?",
+    answer: "albanija"
+};
+var q31 = {
+    question: "Društvena nauka o stanovništvu zove se:",
+    answer: "demografija"
+};
+var q32 = {
+    question: "Kako se zove drzava sa kojom Srbija ima najduzu granicu?",
+    answer: "rumunija"
+};
+var q33 = {
+    question: "Koja je najveća savezna država SAD-a ? ",
+    answer: "aljaska"
+};
+var q34 = {
+    question: "Kako se zove drzava u koju su, nakon II svetskog rata, bezali nacisti u najvecem broju",
+    answer: "argentina"
+};
+var q35 = {
+    question: "Kako se zove reka ciji je kanjon najdublji u Evropi?",
+    answer: "tara"
+};
+var q36 = {
+    question: "U trkama pasa psi jure za?",
+    answer: "zecom"
+};
+var q37 = {
+    question: "Koja zivotinja ima najosetljivije culo sluha?",
+    answer: "slepi mis"
+};
+var q38 = {
+    question: "Kako se zove mesto gde je Karađorđe izabran za vođu Prvog srpskog ustanka?",
+    answer: "takovo"
+};
+var q39 = {
+    question: "Kneza Miloša je 1839. godine na čelu Srbije nasledio:",
+    answer: "milan obrenovic"
+};
+var q40 = {
+    question: "Za zasluge u Kolubarskoj bici 1914. godine čin vojvode dobio je:",
+    answer: "zivojin misic"
+};
+var q41 = {
+    question: "Kako se zove grad odakle potice Kristofor Kolumbo?",
+    answer: "djenova"
+};
+var q42 = {
+    question: "Kako se zove zemlja u kojoj je odgran film 'Braveheart' sa Mel Gibsonom u glavnoj ulozi?",
+    answer: "skotska"
+};
+var q43 = {
+    question: "Kako se zove zemlja koja je prva osvojila Svetsko prvenstvo u fudbalu?",
+    answer: "urugvaj"
+};
+var q44 = {
+    question: "Kako se zove glavni grad Saudijske Arabije?",
+    answer: "rijad"
+};
+var q45 = {
+    question: "kako se zove rat u kom se odigrava radnja filma Saving Private Ryan?",
+    answer: "drugi svetski rat"
+};
+var q46 = {
+    question: "kako se zove drzava kojoj pripada Grenland?",
+    answer: "danska"
+};
+var q47 = {
+    question: "kako se zove drzava u kojoj je rodjen Arnold Svarceneger?",
+    answer: "austrija"
+};
+var q48 = {
+    question: "Prvi glavni grad drevnog Egipta je bio...",
+    answer: "memfis"
+};
+var q49 = {
+    question: "Najgusce naseljen glavni grad u Zapadnoj Evropi je:",
+    answer: "pariz"
+};
+
+
+
+
+var questions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20, q21, q22, q23, q24, q25, q26, q27, q28, q29, q30, q31, q32, q33, q34, q35, q36, q37, q38, q39, q40,q41,q42,q43,q44,q45,q46,q47,q48,q49];
+
+//console.log(questions);
